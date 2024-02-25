508ab1144a871a140dbfe52fe2ca03f58c8bc16f
gallantsa <gallantsa@163.com>
2024-02-25 17:18:19 +0800

    _122_卖股票的最佳时机II

---
+++ mianshi150_1/src/main/java/数组_字符串/_122_卖股票的最佳时机II/Main.java
@@ -0,0 +1,23 @@
+package 数组_字符串._122_卖股票的最佳时机II;
+
+public class Main {
+}
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            if (i - 1 == -1) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            // 这里的代码与上一题的区别是本题允许无限次交易, 而上一题只允许进行一次交易
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+}
