4dab16ae71f1b4319b84d37635972dc1e7e3bd23
gallantsa <gallantsa@163.com>
2024-02-27 16:26:41 +0800

    _42_接雨水: 添加双指针做法

--- mianshi150_1/src/main/java/数组_字符串/_42_接雨水/Main.java
+++ mianshi150_1/src/main/java/数组_字符串/_42_接雨水/Main.java
@@ -30,7 +30,7 @@ class Solution1 {
 }
 
 // 备忘录写法
-class Solution {
+class Solution2 {
     public int trap(int[] height) {
         int n = height.length;
         int res = 0;
@@ -49,6 +49,32 @@ class Solution {
 
         for (int i = 1; i < n - 1; i++) {
             res += Math.min(l_max[i], r_max[i]) - height[i];
+        }
+        return res;
+    }
+}
+
+// 双指针
+class Solution {
+    public int trap(int[] height) {
+        // left 和 right 分别从数组的两端开始，向中间移动。
+        int left = 0, right = height.length - 1;
+        // l_max 和 r_max 分别记录了从左边和右边看到的最高的墙的高度
+        int l_max = 0, r_max = 0;
+
+        int res = 0;
+        while (left < right) {
+            l_max = Math.max(l_max, height[left]);
+            r_max = Math.max(r_max, height[right]);
+
+            // 如果 l_max 较小，那么我们就移动左指针，并且我们知道它能够接的雨水的量就是 l_max - height[left]，因为右边有一个更高的墙 r_max 可以阻止水流出去。
+            if (l_max < r_max) {
+                res += l_max - height[left];
+                left ++;
+            } else {
+                res += r_max - height[right];
+                right --;
+            }
         }
         return res;
     }
