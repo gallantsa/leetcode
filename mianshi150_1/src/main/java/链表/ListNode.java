56d5c76401018eda4f5bc351f90932b7bb7a8fb4
gallantsa <gallantsa@163.com>
2024-03-01 20:05:05 +0800

    链表题型节点类修改

--- mianshi150_1/src/main/java/链表/ListNode.java
+++ mianshi150_1/src/main/java/链表/ListNode.java
@@ -3,8 +3,14 @@ package 链表;
 public class ListNode {
     public int val;
     public ListNode next;
+
     public ListNode(int x) {
         val = x;
         next = null;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
