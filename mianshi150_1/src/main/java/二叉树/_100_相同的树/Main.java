e35eb60a1b69eb3a9238dbe3fa054ab9c8ca8694
gallantsa <gallantsa@163.com>
2024-03-02 20:49:54 +0800

    _100_相同的树

---
+++ mianshi150_1/src/main/java/二叉树/_100_相同的树/Main.java
@@ -0,0 +1,32 @@
+package 二叉树._100_相同的树;
+
+import 二叉树.TreeNode;
+public class Main {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// 属于分解子问题类二叉树问题
+// 解题思路, 判断根节点是否相同, 然后判断左右子树节点是否相同
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
