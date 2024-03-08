75a2ed68acbfcf28a2d9137175b040425ee1ea99
gallantsa <gallantsa@163.com>
2024-03-08 18:55:44 +0800

    _15_三数之和

---
+++ hot100/src/main/java/双指针/_15_三数之和/Main.java
@@ -0,0 +1,61 @@
+package 双指针._15_三数之和;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+}
+
+class Solution1 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                while (j > i + 1 && j < n && nums[j] == nums[j - 1]) j++;
+                if (j >= k) break;
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                while (j < k && j >= i + 1 && nums[j] == nums[j - 1]) j++;
+                if (j >= k) break;
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+}
