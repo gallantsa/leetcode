538eab94655145ee66fb656b0b49d7b573b7a8fb
gallantsa <gallantsa@163.com>
2024-03-09 22:19:12 +0800

    _94_二叉树的中序遍历

---
+++ hot100/src/main/java/二叉树/_94_二叉树的中序遍历/Main.java
@@ -0,0 +1,40 @@
+package 二叉树._94_二叉树的中序遍历;
+
+import 二叉树.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        traverse(root);
+        return res;
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) return;
+
+        traverse(root.left);
+        res.add(root.val);
+        traverse(root.right);
+    }
+}
