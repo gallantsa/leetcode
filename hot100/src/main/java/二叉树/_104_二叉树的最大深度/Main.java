86e01bcb1d0fa994a49315b6a8128b20f4247eec
gallantsa <gallantsa@163.com>
2024-03-09 22:22:45 +0800

    _104_二叉树的最大深度

---
+++ hot100/src/main/java/二叉树/_104_二叉树的最大深度/Main.java
@@ -0,0 +1,32 @@
+package 二叉树._104_二叉树的最大深度;
+
+import 二叉树.TreeNode;
+
+public class Main {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+
+        return Math.max(leftMax, rightMax) + 1;
+    }
+}
