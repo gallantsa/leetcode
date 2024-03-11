f5721c5a6810b8f92bd16f920e507ded6f7fa238
gallantsa <gallantsa@163.com>
2024-03-11 08:52:51 +0800

    _105_从前序与中序遍历序列构造二叉树

---
+++ hot100/src/main/java/二叉树/_105_从前序与中序遍历序列构造二叉树/Main.java
@@ -0,0 +1,51 @@
+package 二叉树._105_从前序与中序遍历序列构造二叉树;
+
+import 二叉树.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+}
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, Integer> valToIndex = new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            valToIndex.put(inorder[i], i);
+        }
+        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+
+        if (preStart > preEnd) return null;
+
+        int rootVal = preorder[preStart];
+        int index = valToIndex.get(rootVal);
+        int leftSize = index - inStart;
+
+        TreeNode root = new TreeNode(rootVal);
+
+        root.left = build(preorder, preStart + 1, preStart + leftSize, inorder, inStart, index - 1);
+        root.right = build(preorder, preStart + leftSize + 1, preEnd, inorder, index + 1, inEnd);
+
+        return root;
+    }
+}
