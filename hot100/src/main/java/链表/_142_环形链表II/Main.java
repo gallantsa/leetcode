2f413e379fd9c9402836a40477c00f29d6119ac9
gallantsa <gallantsa@163.com>
2024-03-12 16:47:19 +0800

    _142_环形链表II

---
+++ hot100/src/main/java/链表/_142_环形链表II/Main.java
@@ -0,0 +1,38 @@
+package 链表._142_环形链表II;
+
+import 链表.ListNode;
+
+public class Main {
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            // 已经成环
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
