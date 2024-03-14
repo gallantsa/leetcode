009b997a1c182222319c72f2422f17285a3aea7a
gallantsa <gallantsa@163.com>
2024-03-14 14:23:20 +0800

    _416_分割等和子集

---
+++ hot100/src/main/java/动态规划/_416_分割等和子集/Main.java
@@ -0,0 +1,36 @@
+package 动态规划._416_分割等和子集;
+
+public class Main {
+}
+
+// dp 数组的定义：dp[i][j] = x 表示，对于前 i 个物品，当前背包的容量为 j 时，若 x 为 true，则说明可以恰好将背包装满，
+// 若 x 为 false，则说明不能恰好将背包装满。
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 == 1) return false;
+        sum /= 2;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+
+        // 背包空间为0, 就是满
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (j - nums[i - 1] < 0) {
+                    // 背包容量不足，不能装入第 i 个物品
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    // 装入或不装入背包
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+}
