c4a18b4764bf6d46b2ba8892ce1e9a7b43512a55
gallantsa <gallantsa@163.com>
2024-03-15 07:56:45 +0800

    _5_最长回文子串

---
+++ hot100/src/main/java/多维动态规划/_5_最长回文子串/Main.java
@@ -0,0 +1,29 @@
+package 多维动态规划._5_最长回文子串;
+
+public class Main {
+}
+
+class Solution {
+    public String longestPalindrome(String s) {
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            // 以 s[i] 为中心的最长回文子串
+            String s1 = palidrome(s, i, i);
+            // 以 s[i] 和 s[i+1] 为中心的最长回文子串
+            String s2 = palidrome(s, i, i + 1);
+            res = res.length() > s1.length() ? res : s1;
+            res = res.length() > s2.length() ? res : s2;
+        }
+        return res;
+    }
+
+    private String palidrome(String s, int l, int r) {
+        // 防止索引越界
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        // 返回以 s[l] 和 s[r] 为中心的最长回文串
+        return s.substring(l + 1, r);
+    }
+}
