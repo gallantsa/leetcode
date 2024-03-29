fde709a13e692dcb46f5bd3baed98147aed87b03
gallantsa <gallantsa@163.com>
2024-03-29 18:59:05 +0800

    _347_前K个高频元素

---
+++ fojiao62/src/main/java/栈与队列/_347_前K个高频元素/Main.java
@@ -0,0 +1,28 @@
+package 栈与队列._347_前K个高频元素;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] ints = solution.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2);
+        System.out.println(Arrays.toString(ints));
+    }
+}
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((o1, o2) -> o2.getValue() - o1.getValue());
+        pq.addAll(map.entrySet());
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = pq.poll().getKey();
+        }
+        return res;
+    }
+}
