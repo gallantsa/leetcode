3e683b4e8251cb02f1ff3d73658558f72e2a3e1a
gallantsa <gallantsa@163.com>
2024-03-30 18:49:32 +0800

    _700_二叉搜索树中的搜索

---
+++ fojiao62/src/main/java/二叉树/_700_二叉搜索树中的搜索/Main.java
@@ -0,0 +1,33 @@
+package 二叉树._700_二叉搜索树中的搜索;
+
+import common.TreeNode;
+
+public class Main {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) return root;
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else if (val > root.val) {
+            return searchBST(root.right, val);
+        }
+        return null;
+    }
+}
