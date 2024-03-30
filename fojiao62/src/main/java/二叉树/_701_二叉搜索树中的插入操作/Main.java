3ceda0233fe4d40372ab255b216ac5d2b720bdb0
gallantsa <gallantsa@163.com>
2024-03-30 19:15:50 +0800

    _701_二叉搜索树中的插入操作

---
+++ fojiao62/src/main/java/二叉树/_701_二叉搜索树中的插入操作/Main.java
@@ -0,0 +1,34 @@
+package 二叉树._701_二叉搜索树中的插入操作;
+
+import common.TreeNode;
+
+public class Main {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // 函数解释: 把val插入到根节点为root的二叉树上, 并返回根节点
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
