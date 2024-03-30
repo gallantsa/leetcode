0766e7769870e5a6234767926f80930ba21f08e4
gallantsa <gallantsa@163.com>
2024-03-30 20:54:36 +0800

    _77_组合_优化

---
+++ fojiao62/src/main/java/回溯算法/_77_组合_优化/Main.java
@@ -0,0 +1,31 @@
+package 回溯算法._77_组合_优化;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+}
+
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        backtracking(n, k, 1);
+        return res;
+    }
+
+    private void backtracking(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = startIndex; i <= n - (k - path.size()) + 1; i++) {
+            path.addLast(i);
+            backtracking(n, k, i + 1);
+            path.removeLast();
+        }
+    }
+}
