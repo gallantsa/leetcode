1ead0abadd95b412e05f3822436ab2c0dec3d68d
gallantsa <gallantsa@163.com>
2024-04-02 00:18:18 +0800

    _647_回文子串

---
+++ fojiao62/src/main/java/动态规划/_647_回文子串/Main.java
@@ -0,0 +1,21 @@
+package 动态规划._647_回文子串;
+
+public class Main {
+}
+
+class Solution {
+    public int countSubstrings(String s) {
+        boolean[][] dp = new boolean[s.length()][s.length()];
+
+        int res = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i <= 1 || dp[i + 1][j - 1])) {
+                    res++;
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return res;
+    }
+}
